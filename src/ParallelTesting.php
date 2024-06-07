d9b44af794a83442fb3823643ec947478319fb90
Kiril Kirkov <kirilkirkov@Kirils-MacBook-Pro.local>
2024-06-07 20:13:16 +0300

    parallel testing improvements

--- src/ParallelTesting.php
+++ src/ParallelTesting.php
@@ -3,7 +3,9 @@
 namespace Src;
 
 use Src\TestsRunner;
+
 use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class ParallelTesting
 {
@@ -28,22 +30,45 @@ class ParallelTesting
         });
      }
 
+     /**
+     * Run the tests in parallel.
+     *
+     * @param array $testFiles List of test files to run
+     */
     public function run(array $testFiles): void
-     {
+    {
         foreach ($testFiles as $file) {
+            // Wait until the count of running processes is less than the maximum
+            while (count($this->processes) >= $this->maxProcesses) {
+                $this->checkProcesses();
+                usleep(100000); // Wait a bit before the next check to reduce overhead
+            }
+
             $process = new Process(['php', 'run-tests.php', '--parallel', $file]);
             $process->start();
-            $this->processes[] = $process; // Store the process for later management
+            $this->processes[] = $process; // Store the process for later reference
         }
 
-        // Make a while loop and show the output of each process until all are finished
+        // Ensure all processes complete
         while (!empty($this->processes)) {
-            foreach ($this->processes as $key => $process) {
-                if (!$process->isRunning()) {
-                    echo $process->getOutput();
-                    unset($this->processes[$key]);
+            $this->checkProcesses();
+            usleep(100000); // Wait a bit before the next check to reduce overhead
+        }
+    }
+
+    /**
+     * Check the status of processes and remove completed ones.
+     */
+    private function checkProcesses(): void
+    {
+        foreach ($this->processes as $key => $process) {
+            if (!$process->isRunning()) {
+                echo $process->getOutput();
+                if ($process->getExitCode() !== 0) {
+                    throw new ProcessFailedException($process);
                 }
+                unset($this->processes[$key]);
             }
         }
-     }
+    }
 }
